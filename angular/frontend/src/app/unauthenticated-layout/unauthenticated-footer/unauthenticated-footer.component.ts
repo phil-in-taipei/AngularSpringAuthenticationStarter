32d57e44db4403f01aeffbbef237b631ae11ead6
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-02-29 17:30:36 +0800

    Router link issue fixed

--- angular/frontend/src/app/unauthenticated-layout/unauthenticated-footer/unauthenticated-footer.component.ts
+++ angular/frontend/src/app/unauthenticated-layout/unauthenticated-footer/unauthenticated-footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-unauthenticated-footer',
   standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './unauthenticated-footer.component.html',
   styleUrl: './unauthenticated-footer.component.css'
 })
--- angular/frontend/src/app/unauthenticated-layout/unauthenticated-header/unauthenticated-header.component.ts
+++ angular/frontend/src/app/unauthenticated-layout/unauthenticated-header/unauthenticated-header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-unauthenticated-header',
   standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './unauthenticated-header.component.html',
   styleUrl: './unauthenticated-header.component.css'
 })
